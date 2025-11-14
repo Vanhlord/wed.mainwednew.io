12d53bb2f813177150a8be59f0daa28c3f503d03
Việt Anh <vivetanh@gmail.com>
2025-11-14 18:30:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -239,7 +239,7 @@ function showPopup() {
   popup.innerHTML = `
     <h2 style="margin:0 0 10px;font-size:1.3em;">Người trẻ tuổi thân mến!</h2>
     <p style="margin:0 0 15px;font-size:0.95em;">Đã cập nhập phiên bản Minecraft mới!<br>
-    Minecraft 1.21.130 Beta<br>
+    - Minecraft 1.21.123<br>
     Các tính năng mới sẽ được thêm...!</p>
     <button style="
       padding:8px 16px;
@@ -278,6 +278,7 @@ function showPopup() {
   `;
   document.head.appendChild(style);
 }
+
 
 
 
