0c9ee298cd3d147a6980c8b4edd12b4cdcea6eba
Việt Anh <vivetanh@gmail.com>
2025-12-11 15:18:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ function showPopup() {
   popup.innerHTML = `
     <h2 style="margin:0 0 10px;font-size:1.3em;">Người trẻ tuổi thân mến!</h2>
     <p style="margin:0 0 15px;font-size:0.95em;">Đã cập nhập phiên bản Minecraft mới!<br>
-    - Minecraft 1.21.124<br>
+    - Minecraft 1.21.130<br>
     Các tính năng mới sẽ được thêm...!</p>
     <button style="
       padding:8px 16px;
@@ -261,6 +261,7 @@ function showPopup() {
 }
 //phần gọi thông báo khi bắt đầu vào web
 document.addEventListener("DOMContentLoaded", showPopup);
+
 
 
 
